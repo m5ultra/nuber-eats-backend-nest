2ca201d39eb95028583d7ea6bd2687672125295c
Dendi <352671309@qq.com>
2021-12-21 23:44:23 +0800

    注册全局组建auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,6 +14,7 @@ import { User } from './users/entities/user.entity'
 import { Restaurant } from './restaurants/entities/restaurants.entity'
 import { AuthModule } from './auth/auth.module'
 import { AuthMiddleware } from './auth/auth.middleware'
+import { MailModule } from './mail/mail.module'
 
 console.log(process.env.NODE_ENV, '~~~env~~~')
 @Module({
@@ -30,6 +31,9 @@ console.log(process.env.NODE_ENV, '~~~env~~~')
         DB_PASSWORD: Joi.string().required(),
         DB_NAME: Joi.string().required(),
         PRIVATE_KEY: Joi.string().required(),
+        MAILGUN_API_KEY: Joi.string().required(),
+        MAILGUN_DOMAIN_NAME: Joi.string().required(),
+        MAILGUN_FROM_EMAIL: Joi.string().required(),
       }),
     }),
     GraphQLModule.forRoot({
@@ -53,6 +57,11 @@ console.log(process.env.NODE_ENV, '~~~env~~~')
     UsersModule,
     AuthModule.forRoot({
       privateKey: process.env.PRIVATE_KEY,
+    }),
+    MailModule.forRoot({
+      apiKey: process.env.MAILGUN_API_KEY,
+      domain: process.env.MAILGUN_DOMAIN_NAME,
+      fromEmail: process.env.MAILGUN_FROM_EMAIL,
     }),
   ],
 })
--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,22 +1,22 @@
 import { DynamicModule, Global, Module } from '@nestjs/common'
-import { AuthService } from './auth.service'
-import { AuthModuleOptions } from './auth-module-options.interface'
 import { CONFIG_OPTIONS } from '../common/common.constants'
+import { MailService } from './mail.service'
+import { MailModuleOptions } from './mail.interface'
 
 @Module({})
 @Global()
-export class AuthModule {
-  static forRoot(options: AuthModuleOptions): DynamicModule {
+export class MailModule {
+  static forRoot(options: MailModuleOptions): DynamicModule {
     return {
-      module: AuthModule,
+      module: MailModule,
       providers: [
         {
           provide: CONFIG_OPTIONS,
           useValue: options,
         },
-        AuthService,
+        MailService,
       ],
-      exports: [AuthService],
+      exports: [MailService],
     }
   }
 }
